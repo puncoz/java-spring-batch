f5ef7f6f196963f46e1c85b7fdd601f7448678ba
Puncoz Nepal <puncoz138@gmail.com>
2024-01-13 01:08:20 +0900

    spring boot project

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.practice"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/practice/javaspringbatch/JavaSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.practice.javaspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBatchApplication.class, args);
+    }
+
+}
